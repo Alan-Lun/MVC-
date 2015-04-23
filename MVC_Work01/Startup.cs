cfc511799aa74112c3cbaced589dcac56d8b3d49
alanlun <s52076663135@gmail.com>
2015-04-23 20:36:45 +0800

    一、完成自訂的 SQL Server 檢視表，並且匯入 EDMX

---
+++ MVC_Work01/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_Work01.Startup))]
+namespace MVC_Work01
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
